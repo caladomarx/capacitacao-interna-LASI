9d1e11acb1a1afe7d21f49916334e37092421401
caladomarx <raffaelmarxcaladomendes2@gmail.com>
2026-05-17 16:57:11 +0000

    inserção de detalhamento acerca do que será feito no códico + validação do funcionamento das extenções instaladas

---
+++ Sistema-de-Biblioteca-Pessoal.c
@@ -0,0 +1,19 @@
+/*
+====Sistema de Biblioteca Pessoal====
+
+### Objetivo
+
+Criar um sistema para gerenciamento de livros.
+
+### Funcionalidades
+
+* cadastrar livros;
+* listar livros;
+* buscar livros;
+* marcar livros como lidos;
+* remover livros. */
+
+#include <stdio.h>
+int main(){
+    printf("ten tereten tere... 123 ai ai ai aiaiai ai ai aiai uh uh uh uh uh uh... ê ê uh uh uh uh uh uh uh hehe bip bip bip-bip bip bi-oh yeah bip bip bip bip bip bip bip bi-fica de quato bip bip fica de- ten ten ten ten ten fica de quato ten ten ten fica de-ten ten ten-rai-rai ten ten ten rai-rai ten terereten... ten ten terereten... ai ai tenteretenteretenteretentereten-fica de quato weck weck fica de quato weckweckweck fica de quato weck ai fica de quato weckwe-123 ei 12 12 aiaiai bley bleybley aiaiai bley ui ui ui uiuiui ui 123 ui ui ui uiuiui não para vem-no-sapatin não pa-não para vem no sapatin ai ai!");
+}
